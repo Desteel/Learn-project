643b78d52aaf8d023effe93786c4bdf07c1c093b
Desteel <alexgowargame@yandex.ru>
2020-10-02 03:09:42 +0400

    [refactor] separated route link to common components

--- client/src/components/Navigation/Navigation.tsx
+++ client/src/components/Navigation/Navigation.tsx
@@ -1,14 +1,32 @@
 import React from "react";
-import NavigationItem from "./NavigationItem";
+import RouteLink from "./RouteLink";
 import { NavigationRoutes } from "constants/navigation";
+
+const NavigationItems = [
+  {
+    path: NavigationRoutes.Main,
+    title: "Main",
+    exact: true
+  },
+  {
+    path: NavigationRoutes.Catalog,
+    title: "Catalog"
+  },
+  {
+    path: NavigationRoutes.Personal,
+    title: "Personal"
+  }
+];
 
 function Navigation() {
   return (
     <nav>
       <ul>
-        <NavigationItem exact path={NavigationRoutes.Main} title="Main" />
-        <NavigationItem path={NavigationRoutes.Catalog} title="Catalog" />
-        <NavigationItem path={NavigationRoutes.Personal} title="Personal" />
+        {NavigationItems.map(({ path, title, exact }) => (
+          <li key={title}>
+            <RouteLink title={title} path={path} exact={exact} />
+          </li>
+        ))}
       </ul>
     </nav>
   );
--- client/src/components/Navigation/RouteLink/RouteLink.tsx
+++ client/src/components/Navigation/RouteLink/RouteLink.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 import { Link, useRouteMatch } from "react-router-dom";
 
-type NavigationItemProps = {
+type RouteLinkProps = {
   title: string;
   path: string;
 
   exact?: boolean;
 };
 
-function NavigationItem({ path, title, exact }: NavigationItemProps) {
+function RouteLink({ path, title, exact }: RouteLinkProps) {
   let match = useRouteMatch({
     path,
     exact
   });
 
   return (
-    <li className={match ? "nav__item--active" : ""}>
+    <div className={match ? "nav__item--active" : ""}>
       {match && "> "}
       <Link to={path}>{title}</Link>
-    </li>
+    </div>
   );
 }
 
-export default NavigationItem;
+export default RouteLink;
