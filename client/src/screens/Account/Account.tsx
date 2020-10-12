db766f29b4adc8fe4c01d1372c7d6b1baae6633c
Desteel <alexgowargame@yandex.ru>
2020-10-13 01:42:50 +0400

    [feat] implemented base account screen

---
+++ client/src/screens/Account/Account.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Account() {
+  return (
+    <main>
+      <h1>Account</h1>
+
+      <div>
+        <h2>Add product </h2>
+        <form></form>
+      </div>
+    </main>
+  );
+}
+
+export default Account;
--- client/src/screens/index.ts
+++ client/src/screens/index.ts
@@ -2,3 +2,4 @@ export { default as Main } from "./Main";
 export { default as Catalog } from "./Catalog";
 export { default as Personal } from "./Personal";
 export { default as NotFound } from "./NotFound";
+export { default as Account } from "./Account";
